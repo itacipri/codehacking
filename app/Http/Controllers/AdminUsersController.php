28dc142c8888d3bb98342f8c5eb6bc1b27284948
itacipri <itacipri@gmail.com>
2017-09-22 21:33:54 -0300

    All Ok but Middleware

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersEditRequest;
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\Role;
 use App\User;
 use Carbon\Carbon;
@@ -21,8 +23,6 @@ class AdminUsersController extends Controller
     {
         Carbon::setLocale('pt-br');
         $users = User::all();
-
-
         return view('admin.users.index', compact('users'));
     }
 
@@ -46,7 +46,38 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        return $request->all();
+        //User::create($request->all());
+
+        if(trim($request->password == '')) {
+
+            $input = $request->except('password');
+
+        } else {
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }
+
+
+        //$input = $request->all();
+
+        if($file = $request->file('photo_id')) {
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+
+
+        }
+
+        $input['password'] = bcrypt($request->password);
+
+        User::create($input);
+
+        return redirect('/admin/users');
     }
 
     /**
@@ -68,7 +99,11 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        $roles = Role::pluck('name', 'id')->all();
+
+        return view('admin.users.edit', compact('user', 'roles'));
     }
 
     /**
@@ -78,11 +113,39 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
-        //
-    }
 
+        $user = User::findOrFail($id);
+
+        if(trim($request->password == '')) {
+
+            $input = $request->except('password');
+
+        } else {
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+        }
+
+        //$input = $request->all();
+
+        if ($file = $request->file('photo_id')) {
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file' => $name]);
+
+            $input['photo_id'] = $photo->id;
+
+        }
+
+        $user->update($input);
+
+
+        return redirect('admin/users');
+    }
     /**
      * Remove the specified resource from storage.
      *
