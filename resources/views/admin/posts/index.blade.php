c2931adb450140ad55adf5eb73bc6933b80f9163
itacipri <itacipri@gmail.com>
2017-09-24 12:42:51 -0300

    able to create posts with dyn categories

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -29,7 +29,7 @@
       <tr>
         <td>{{$post->id}}</td>
         <td>{{$post->user->name}}</td>
-        <td>{{$post->category_id}}</td>
+        <td>{{$post->category ? $post->category->name : 'No category'}}</td>
         <td><img height="50" src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400'}}"></td>
         <td>{{$post->title}}</td>
         <td>{{$post->body}}</td>
